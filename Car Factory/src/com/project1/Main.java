00afa4d8dc1f1e189be89494806c815406101d2a
nobletk <80173312+nobletk@users.noreply.github.com>
2021-11-18 12:34:50 +0100

    Update Main.java

--- Car Factory/src/com/project1/Main.java
+++ Car Factory/src/com/project1/Main.java
@@ -3,10 +3,31 @@ package com.project1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	 C1 c1= new C1();
+        c1.modelName="C1";
+        c1.engineSize="123";
+        c1.color="Black";
+        c1.wheel="4";
+        c1.ABS="yes";
 
-        // static print (IK MyIK){
-        //    MyIk.Display();
+        C2 c2= new C2();
+        c2.modelName="C2";
+        c2.engineSize="345";
+        c2.color="Silver";
+        c2.GPS="Yes";
+        c2.leatherSeat="Yes";
+
+
+        Scanner MyIK=new Scanner(System.in);
+        System.out.println("Ik model:");
+        String n= MyIK.next();
+
+
+        if(n.equalsIgnoreCase(c1.modelName)) {
+            c1.Display();
+        }
+        if(n.equalsIgnoreCase(c2.modelName)) {
+            c2.Display();
         }
     }
 }
