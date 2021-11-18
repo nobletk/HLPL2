c3f6499eba94e6444dea5eb95c3f6a464a7689fd
nobletk <80173312+nobletk@users.noreply.github.com>
2021-11-18 12:37:55 +0100

    Update Main.java

--- comp app week 9/src/com/project1/Main.java
+++ comp app week 9/src/com/project1/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        ArrayList<customer> allCustomer = new ArrayList<customer>();
+        ArrayList<customer> allCus = new ArrayList<customer>();
         ArrayList<employee> allEmp = new ArrayList<employee>();
 
         customer c1 = new customer();
